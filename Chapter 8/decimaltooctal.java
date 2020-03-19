3d0f29e599148c1825ccbd3e12d735f4440756aa
Aiden-Taylor <57740540+Aiden-Taylor@users.noreply.github.com>
2020-03-18 17:08:21 -0700

    Update decimaltooctal.java

--- Chapter 8/decimaltooctal.java
+++ Chapter 8/decimaltooctal.java
@@ -3,10 +3,10 @@ public class decimaltooctal{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int x = scan.nextInt();
-        System.out.println(aidenissosmartandcoolhecanprograminjavalikeagodbutnotaswellasjackson(x, 0, 1+(int)(Math.log(x)/Math.log(8))));
+        System.out.println(convert(x, 0, 1+(int)(Math.log(x)/Math.log(8))));
         scan.close();
     }
-    public static int aidenissosmartandcoolhecanprograminjavalikeagodbutnotaswellasjackson(int a, int b, int c){
+    public static int convert(int a, int b, int c){
         c--;
         if (a == 0){
             return b;
@@ -14,7 +14,7 @@ public class decimaltooctal{
         else{
             b += Math.pow(10, c)*a/(Math.pow(8, c));
             a = a%(int)(Math.pow(8, c));
-            return aidenissosmartandcoolhecanprograminjavalikeagodbutnotaswellasjackson(a, b, c);
+            return convert(a, b, c);
         }
     }
 }
